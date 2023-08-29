e62e4a69dce228f97ca4f9e04c50d19c5ab7da91
Replit user <>
2023-08-28 21:27:08 -0500

    upload work

--- myApp.js
+++ myApp.js
@@ -1,5 +1,39 @@
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`)
+  next();
+})
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html")
+});
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.use("/json", (req, res) => {
+  const message = "Hello json";
+  const modifiedMessage = process.env['MESSAGE_STYLE'] === 'uppercase' ? message.toUpperCase() : message;
+  res.json({ message: modifiedMessage })
+})
+
+app.get('/now', (req, res, next) => {
+  req.time = new Date().toString();
+  next()
+}, (req, res) => {
+  res.send({ time: req.time })
+})
+
+app.get("/:word/echo", (req, res, next) => {
+  res.send({ echo: req.params.word });
+  next();
+});
+
+app.route('/name').get((req, res, next) => { res.send({ name: `${req.query.first} ${req.query.last}` }); next(); }).post((req, res, next) => { res.json({ name: `${req.body.first} ${req.body.last}` }); next(); });
 
 
 
@@ -27,13 +61,4 @@ let app = express();
 
 
 
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
